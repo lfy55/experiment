606b6ff066868a2e3f14fae009ba40a6a1b85583
songqiankun <sqk_55@163.com>
2017-12-12 00:18:27 +0800

    fix bar chart

--- src/components/charts/linearBar.vue
+++ src/components/charts/linearBar.vue
@@ -5,46 +5,65 @@
 </template>
 
 <script>
-import echarts from 'echarts'
+import echarts from "echarts";
 
 export default {
-  name: 'linearBar',
+  name: "linearBar",
   data() {
     return {
       barData: [],
       axis: [],
-      myChart: null,
-    }
+      myChart: null
+    };
   },
   mounted() {
-    this.myChart = echarts.init(this.$refs.linearBar)
-    this.initData()
-    this.drawChart()
+    this.myChart = echarts.init(this.$refs.linearBar);
+    this.initData();
+    this.drawChart();
   },
   methods: {
     initData() {
-      this.axis = []
-      this.barData = []
+      this.axis = [];
+      this.barData = [];
       for (let index = 0; index < 18; index++) {
-        this.axis.push(`项目${index}`)
-        this.barData.push(~~(Math.random() * 30))
+        this.axis.push(`项目${index}`);
+        this.barData.push(~~(Math.random() * 30));
       }
     },
     drawChart() {
-      let maxData = 0, length = this.axis.length
-      this.barData.forEach(item => item > maxData ? maxData = item : 0)
-      maxData = maxData * 1.1
+      let maxData = 0,
+        length = this.axis.length;
+      this.barData.forEach(item => (item > maxData ? (maxData = item) : 0));
+      maxData = maxData * 1.1;
 
       let option = {
-        backgroundColor: '#999999',
+        backgroundColor: "#999999",
         title: {
-          text: '特性示例：渐变色 阴影 点击缩放',
+          show: false
+        },
+        tooltip: {
+          show: true
         },
         xAxis: {
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          axisLabel: {
+            show: false
+          },
+          splitLine: {
+            show: false
+          }
+        },
+        yAxis: {
           data: this.axis,
+          inverse: true,
           axisLabel: {
             textStyle: {
-              color: '#fff'
+              color: "#fff"
             }
           },
           axisTick: {
@@ -52,53 +71,65 @@ export default {
           },
           axisLine: {
             show: false
-          },
-        },
-        yAxis: {
-          axisLine: {
-            show: false,
-          },
-          axisTick: {
-            show: false,
-          },
-          axisLabel: {
-            show: false,
-          },
-          splitLine: {
-            show: false,
-          },
+          }
         },
         series: [
-          { // For shadow
-            type: 'bar',
+          {
+            // For shadow
+            type: "bar",
             itemStyle: {
-              normal: { color: 'rgba(0,0,0,0.05)' }
+              normal: { color: "rgba(0,0,0,0.05)" }
             },
-            barGap: '-100%',
-            barCategoryGap: '80%',
-            data: new Array(length).fill(maxData),
+            barGap: "-100%",
+            barCategoryGap: "80%",
+            data: this.barData.map((item, index) => {
+              return {
+                value: maxData,
+                real: item,
+                label: {
+                  normal: {
+                    color: index > 5 ? '#fff' : '#000'
+                  },
+                },
+              };
+            }),
+            label: {
+              normal: {
+                show: true,
+                position: "insideRight",
+                formatter: function (params) {
+                  return params.data.real;
+                }
+              }
+            },
+            tooltip: {
+              show: false
+            },
             animation: false
           },
           {
-            type: 'bar',
+            type: "bar",
             itemStyle: {
               normal: {
                 color: {
-                  type: 'linear',
-                  x: 0, y: 0, x2: 0, y2: 1,
+                  type: "linear",
+                  x: 0,
+                  y: 0,
+                  x2: 0,
+                  y2: 1,
                   colorStops: [
-                    { offset: 0, color: '#01a2d8' },
-                    { offset: 1, color: '#024465' },
+                    { offset: 0, color: "#01a2d8" },
+                    { offset: 1, color: "#024465" }
                   ]
                 }
-              },
+              }
             },
             data: this.barData
           }
         ]
-      }
+      };
 
-      this.myChart.setOption(option)
+      this.myChart.setOption(option);
     }
   }
 }
